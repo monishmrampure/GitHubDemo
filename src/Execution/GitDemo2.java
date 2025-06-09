ff686177b6577bd9c24ff7de1f317f58ce3659bc
Monish <monishmrampure@gmail.com>
2025-06-09 11:06:36 +0530

    Architect US Developed

--- src/Execution/GitDemo2.java
+++ src/Execution/GitDemo2.java
@@ -11,8 +11,16 @@ public class GitDemo2 {
 		System.out.println("Checking the changes in the Gitdemo2");
 	}
 	
+	@Test
 	public void getData2() {
 		System.out.println("execution of webautomation");
 		System.out.println("Working on Git/github automation for selenium");
+	}
+	
+	@Test
+	public void architechChange() {
+		System.out.println("Changing the entire framework");
+		System.out.println("Creating the Subbranch Develop");
+		System.out.println("Changes are done in the Subbranch in GitHub");
 	}
 }
